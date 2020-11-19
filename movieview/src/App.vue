fac0fcda398707c1b9727c9ca393bd4c644dd068
Eugene <2422@hanmail.net>
2020-11-19 14:39:29 +0900

    axios example

---
+++ movieview/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <User/>
+  </div>
+</template>
+
+<script>
+import User from "@/components/User";
+
+export default {
+  components: {
+    User
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ movieview/src/components/Paging.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul class="paging" style="padding-inline-start:0px;">
+    <li v-for="(page, idx) in totalPage" :key="idx" @click="movepage(page)" style="display:inline-block; padding : 5px;">{{page}}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: ["totalPage"],
+  data() {
+    return {};
+  },
+  methods: {
+    movepage(page) {
+      this.$emit("movepage", page);
+    }
+  }
+};
+</script>
