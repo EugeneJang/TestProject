52ea513963fa85ddc7ff3bf1089de8f3b165c6f9
Eugene <2422@hanmail.net>
2020-11-23 15:52:08 +0900

    v-bind add

--- movieview/src/components/Paging.vue
+++ movieview/src/components/Paging.vue
@@ -1,6 +1,7 @@
 <template>
   <ul class="paging" style="padding-inline-start:0px;">
-    <li v-for="(page, idx) in totalPage" :key="idx" @click="movepage(page)" style="display:inline-block; padding : 5px;">{{page}}</li>
+    <button v-for="(page, idx) in totalPage" :key="idx" @click="movepage(page)" style="display:inline-block; padding : 5px; margin : 5px;">{{ page }}</button>
+    <!--<li v-for="(page, idx) in totalPage" :key="idx" @click="movepage(page)" style="display:inline-block; padding : 5px;" v-on="mouseover: doMouseOver">{{page}}</li>-->
   </ul>
 </template>
 
@@ -8,11 +9,16 @@
 export default {
   props: ["totalPage"],
   data() {
+    console.log("childe.vue Paging Data init");
     return {};
   },
   methods: {
     movepage(page) {
+      console.log("childe.vue method movepage return");
       this.$emit("movepage", page);
+    },
+    doMouseOver:()=>{
+
     }
   }
 };
--- movieview/src/components/User.vue
+++ movieview/src/components/User.vue
@@ -1,8 +1,27 @@
 <template>
   <div>
     <div v-for="(user, idx) in users" :key="idx" style="display:inline-block; margin:10px">
-      <h2>{{user.first_name}}</h2>
+      <div v-if="user.id < 7">
+        <h2 v-for="(col, index) in colors" :key="index">
+          <h2 v-if="user.id === col.id" v-bind:style="{color:[col.color]}">
+          <!-- v-bind:style="color.color">  -->
+            {{ user.first_name }}
+          </h2>
+        </h2>
+      </div>
+      <div v-else>
+        <h2 v-for="(col, index) in colors" :key="index">
+          <h2 v-if="user.id-6 === col.id" v-bind:style="{color:[col.color]}">
+          <!-- v-bind:style="color.color">  -->
+            {{ user.first_name }}
+          </h2>
+        </h2>
+        <!-- {{ user.first_name }} -->
+      </div>
+      <!-- <h2 v-bind:id=" user.id "> {{ user.first_name }}</h2> -->
+      <h2>{{ user.last_name }}</h2>
       <img :src="'https://i.pinimg.com/474x/7d/56/56/7d5656879b5d6ed45779f89c4e89c91a.jpg'" style="width:100px; height:100px" alt />
+      <h2>{{ user.email }}</h2>
     </div>
     <Paging :totalPage="totalPage" @movepage="movepage" />
   </div>
@@ -16,12 +35,14 @@ export default {
   data() {
     return {
       users: null,
+      colors : null,
       totalPage: null,
       pageNum: 1
     };
   },
   methods: {
     fetchData(pageNum) {
+      console.log("User.vue Call fetchData() axios");
       axios
         .get("https://reqres.in/api/users?page=" + pageNum)
         .then(res => {
@@ -34,11 +55,21 @@ export default {
         });
     },
     movepage(page) {
+      console.log("User.vue Call movepage()");
       this.fetchData(page);
     }
   },
   created() {
-    this.fetchData(this.pageNum);
+    console.log("User.vue created()");
+    
+    axios
+        .get("https://reqres.in/api/unknown")
+        .then(res =>{
+          this.colors = res.data.data;
+          console.log(res.data);
+          // alert(colors);
+        this.fetchData(this.pageNum);
+    });
   },
   components: {
     Paging
